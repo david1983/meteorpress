64dc6dd78c6f7c4e5f25d116a7dbd3abb21a7ed1
davide <davideandreazzini@gmail.com>
2015-11-17 09:15:35 +0000

    first commit

--- client/init.js
+++ client/init.js
@@ -1,3 +1,15 @@
 Meteor.startup(function() {
     Uploader.uploadUrl = Meteor.absoluteUrl("upload"); // Cordova needs absolute URL
 });
+
+ShareIt.init({
+    siteOrder: ['facebook', 'twitter', 'googleplus', 'pinterest'],
+    sites: {
+        'facebook': {
+            'appId': '1580484175528065',
+            'version': 'v2.3'
+        }
+    },
+    iconOnly: true,
+    applyColors: true
+});
